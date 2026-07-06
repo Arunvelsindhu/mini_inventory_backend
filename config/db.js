aede27bc23140abc1849f6831e2449d9c59e6e37
Arunvel <arunvel1412@gmail.com>
2026-07-06 21:00:47 +0530

    feat: use environment variables for database

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Arunvel@2001",
-    database: "inventory_db"
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT
 });
 
 connection.connect((err) => {
