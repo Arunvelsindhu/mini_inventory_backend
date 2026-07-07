7894db2b6f4fdb16ee67d23f8b370a1bd2d8fe79
Arunvel <arunvel1412@gmail.com>
2026-07-07 10:49:14 +0530

    Restore backend

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
 const express = require("express");
+const cors = require("cors");
+
+const db = require("./config/db");
+
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
+const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
+// Home Route
 app.get("/", (req, res) => {
-    res.send("HELLO FROM RENDER");
+    res.send("🚀 Inventory Management API is Running...");
 });
 
-app.get("/test", (req, res) => {
-    res.send("TEST OK");
-});
+// Routes
+app.use("/products", productRoutes);
+app.use("/", userRoutes);
+app.use("/dashboard", dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log("SERVER STARTED");
+    console.log(`🚀 Server is running on port ${PORT}`);
 });
