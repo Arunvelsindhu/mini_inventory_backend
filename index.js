9d7c2dfd0c7762da95f7fc2904d48452f4b034e7
Arunvel <arunvel1412@gmail.com>
2026-07-07 10:44:29 +0530

    Testing minimal Express app

--- index.js
+++ index.js
@@ -1,39 +1,17 @@
 const express = require("express");
-const cors = require("cors");
-
-const db = require("./config/db");
-
-const productRoutes = require("./routes/productRoutes");
-const userRoutes = require("./routes/userRoutes");
-const dashboardRoutes = require("./routes/dashboardRoutes");
 
 const app = express();
 
-// =========================
-// Middleware
-// =========================
-app.use(cors());
-app.use(express.json());
-
-// =========================
-// Routes
-// =========================
-app.use("/products", productRoutes);
-app.use("/", userRoutes);
-app.use("/dashboard", dashboardRoutes);
-
-// =========================
-// Home Route
-// =========================
 app.get("/", (req, res) => {
-    res.send("🚀 Inventory Management API is Running...");
+    res.send("HELLO FROM RENDER");
 });
 
-// =========================
-// Server
-// =========================
+app.get("/test", (req, res) => {
+    res.send("TEST OK");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`🚀 Server is running on port ${PORT}`);
+    console.log("SERVER STARTED");
 });
